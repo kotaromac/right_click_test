b9258e4ac4f3e15c251ca37aaf45c9e337985e9b
Kotaro Yama <yuzaburo0726@gmail.com>
2015-03-27 23:19:14 +0900

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Add mousdown event to the image
+// When you save the image, a stupid alert will appear.
+
 var img = document.getElementsByTagName('img').item(0);
 img.addEventListener('mousedown', function(e){
 	if (e.button == 2) {
